4db9bd0515148f9717ab764591e2209d733020c7
maynor96 <mayeral96@gmail.com>
2018-08-20 04:27:34 -0600

    cambios diseños

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Carousel 
+
+$(function() {
+    'use strict';
+    
+      $('.carousel .carousel-item[data-src]').each(function() {
+          var $this = $(this);
+  
+          $this.prepend([
+              '<div style="background-image: url(', $this.attr('data-src'), ')"></div>'
+          ].join(''));
+      });
+  });
+
+
+//   radio
